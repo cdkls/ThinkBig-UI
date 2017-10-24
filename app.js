dd8dbac1fe3847d91dce1d52e55e484aa6d51e32
Satish Kallepalli <satish_kallepalli@gap.com>
2017-10-24 03:06:49 -0700

    Adding package json

---
+++ app.js
@@ -0,0 +1,16 @@
+var express  = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// parses request body and populates request.body
+app.use(bodyParser.json());
+
+// Where to serve static content
+app.use(express.static('.'));
+
+app.get('/', function (req, res) {
+    res.send("Welcome to Think Big");
+    });
+
+console.log("Think Big UI listening on port 3000");
+app.listen(3000, function() {});
